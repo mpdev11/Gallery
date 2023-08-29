dc8aeb2c5acfb21086491f68b36ce756bacdf959
Grujica Pantic <grujicapantic95@gmail.com>
2023-08-29 10:47:45 +0200

    Main components and content created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DrawingsComponent } from './drawings/drawings.component';
+import { PaintingsComponent } from './paintings/paintings.component';
+import { HomeComponent } from './core/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'paintings', component: PaintingsComponent },
+  { path: 'drawings', component: DrawingsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/paintings/paintings.component.ts
+++ src/app/paintings/paintings.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-paintings',
   templateUrl: './paintings.component.html',
-  styleUrls: ['./paintings.component.scss']
+  styleUrls: ['./paintings.component.scss'],
 })
 export class PaintingsComponent implements OnInit {
+  paintings: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
