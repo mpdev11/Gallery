e27a4601622eaec99f2e4757fb61e15deedbaf39
Grujica Pantic <grujicapantic95@gmail.com>
2023-08-29 09:55:29 +0200

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
+import { PaintingsComponent } from './paintings/paintings.component';
+import { PaintingComponent } from './painting/painting.component';
+import { DrawingsComponent } from './drawings/drawings.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    PaintingsComponent,
+    PaintingComponent,
+    DrawingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
