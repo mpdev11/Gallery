4ca1dd632f84e64c1096b76093a171c5f8841933
Grujica Pantic <grujicapantic95@gmail.com>
2023-10-05 16:44:22 +0200

    Major design changes

--- src/app/painting/painting.component.ts
+++ src/app/painting/painting.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-painting',
   templateUrl: './painting.component.html',
-  styleUrls: ['./painting.component.scss']
+  styleUrls: ['./painting.component.scss'],
 })
 export class PaintingComponent implements OnInit {
+  @Input()
+  imgIndex: Number = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/paintings/paintings.component.ts
+++ src/app/paintings/paintings.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paintings.component.scss'],
 })
 export class PaintingsComponent implements OnInit {
-  paintings: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  paintings: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+  ];
   constructor() {}
 
   ngOnInit(): void {}
